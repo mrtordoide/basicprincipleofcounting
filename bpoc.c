d36e636af8f9bb532a243e388d5e72256132cc2b
Amit Shrestha <amitshrestha37@gmail.com>
2016-06-30 19:37:36 +0545

    main file

---
+++ bpoc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+// Currently contains only the letter arrangement ways.
+int main(){
+  int n, aw=1;
+  printf("Enter the number of letters to be arranged:");
+  scanf("%d", &n);
+  for (int i=1; i<=n; i++){
+    aw = aw*i;
+  }
+  printf("\nA group of %d letters can be arranged in %d different ways.\n", n, aw);
+}
